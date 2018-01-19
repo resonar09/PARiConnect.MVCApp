e2abd65f34f12f2ae5a34ef056ab7b4f510298ec
resonar09 <darrenperez@gmail.com>
2018-01-19 18:57:41 -0500

    Added AssessmentReview ViewComponent and fixed sorting

---
+++ ViewComponents/AssessmentReviewViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PARiConnect.MVCApp.Services;
+using PARiConnect.MVCApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARiConnect.MVCApp.ViewComponents
+{
+    public class AssessmentReviewViewComponent : ViewComponent
+    {
+        private IAssessmentReviewData _assessmentReviewData;
+
+        public AssessmentReviewViewComponent(IAssessmentReviewData assessmentReviewData)
+        {
+            _assessmentReviewData = assessmentReviewData;
+        }
+        public IViewComponentResult Invoke(bool completed)
+        {
+            var model = new HomeIndexViewModel();
+            var assessmentReviews = _assessmentReviewData.GetAllAsync().Result;
+            model.AssessmentReview = assessmentReviews.Where(x => x.Completed == completed);
+            return View("AssessmentReview", model);
+        }
+    }
+}
