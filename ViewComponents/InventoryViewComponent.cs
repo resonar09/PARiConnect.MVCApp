77eb8536cd83442a30172292c6afecf7642ed902
resonar09 <darrenperez@gmail.com>
2018-01-31 19:13:48 -0500

    Added Inventory, fixed style, fixed first last sort, added popper.js

---
+++ ViewComponents/InventoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PARiConnect.MVCApp.Services;
+using PARiConnect.MVCApp.ViewModels;
+using System.Linq;
+
+namespace PARiConnect.MVCApp.ViewComponents
+{
+    public class InventoryViewComponent : ViewComponent
+    {
+        private IAssessmentReviewData _assessmentReviewData;
+
+        public InventoryViewComponent(IAssessmentReviewData assessmentReviewData)
+        {
+            _assessmentReviewData = assessmentReviewData;
+        }
+        public IViewComponentResult Invoke()
+        {
+
+            return View("Inventory");
+        }
+    }
+}
