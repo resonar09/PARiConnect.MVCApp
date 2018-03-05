462f1b1edf90efa24930dfb6dd90cefa94794be2
resonar09 <darrenperez@gmail.com>
2018-03-05 09:32:13 -0500

    Update filtering

--- Services/AssessmentReviewDataMock.cs
+++ Services/AssessmentReviewDataMock.cs
@@ -28,6 +28,20 @@ namespace PARiConnect.MVCApp.Services
                      StatusKey = 6
                 },
                 new AssessmentReview {
+                    ClientId = 1,
+                    ClientName ="Sam Smith",
+                    Assessment = "CAB Parent Form",
+                    Updated = DateTime.Parse("12/12/2016"),
+                     StatusKey = 13
+                }, 
+                new AssessmentReview {
+                    ClientId = 2,
+                    ClientName ="Liz Evins",
+                    Assessment = "CAD Self Form",
+                    Updated = DateTime.Parse("12/08/2017"),
+                     StatusKey = 13
+                },
+                new AssessmentReview {
                     ClientId = 3,
                     ClientName ="Mike Smith",
                     Assessment = "Brief Student Form",
@@ -48,19 +62,40 @@ namespace PARiConnect.MVCApp.Services
                     Updated = DateTime.Parse("1/22/2018"),
                      StatusKey = 5
                 },
-                new AssessmentReview {
-                    ClientId = 6,
-                    ClientName ="Steve Crowder",
+                                new AssessmentReview {
+                    ClientId = 3,
+                    ClientName ="Mike Smith",
                     Assessment = "Brief Student Form",
                     Updated = DateTime.Parse("12/22/2017"),
                      StatusKey = 6
                 },
                 new AssessmentReview {
-                    ClientId = 7,
-                    ClientName ="Dave Rubin",
+                    ClientId = 4,
+                    ClientName ="Tab Johnson",
                     Assessment = "Spectra Form",
                     Updated = DateTime.Parse("1/12/2018"),
-                     StatusKey = 5
+                     StatusKey = 13
+                },
+                new AssessmentReview {
+                    ClientId = 5,
+                    ClientName ="Chris Peterson",
+                    Assessment = "Spectra Form",
+                    Updated = DateTime.Parse("1/22/2018"),
+                     StatusKey = 13
+                },
+                new AssessmentReview {
+                    ClientId = 6,
+                    ClientName ="Steve Crowder",
+                    Assessment = "Brief Student Form",
+                    Updated = DateTime.Parse("12/22/2017"),
+                     StatusKey = 13
+                },
+                new AssessmentReview {
+                    ClientId = 7,
+                    ClientName ="Dave Rubin",
+                    Assessment = "AAB Standard From",
+                    Updated = DateTime.Parse("1/12/2018"),
+                     StatusKey = 13
                 },
                 new AssessmentReview {
                     ClientId = 8,
@@ -77,6 +112,13 @@ namespace PARiConnect.MVCApp.Services
                      StatusKey = 6
                 },
                 new AssessmentReview {
+                    ClientId = 6,
+                    ClientName ="Steve Crowder",
+                    Assessment = "Brief Student Form",
+                    Updated = DateTime.Parse("1/24/2017"),
+                     StatusKey = 13
+                },
+                new AssessmentReview {
                     ClientId = 7,
                     ClientName ="Dave Rubin",
                     Assessment = "Spectra Form",
@@ -84,11 +126,26 @@ namespace PARiConnect.MVCApp.Services
                      StatusKey = 5
                 },
                 new AssessmentReview {
+                    ClientId = 7,
+                    ClientName ="Dave Rubin",
+                    Assessment = "Spectra Form",
+                    Updated = DateTime.Parse("1/25/2018"),
+                     StatusKey = 13
+                },
+                new AssessmentReview {
                     ClientId = 8,
                     ClientName ="Gavin McGinnis",
                     Assessment = "Spectra Form",
                     Updated = DateTime.Parse("1/26/2018"),
                      StatusKey = 5
+                }
+                ,
+                new AssessmentReview {
+                    ClientId = 8,
+                    ClientName ="Gavin McGinnis",
+                    Assessment = "Spectra Form",
+                    Updated = DateTime.Parse("1/26/2018"),
+                    StatusKey = 13
                 }
             };
         }
