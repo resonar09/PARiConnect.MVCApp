aa430bfa6f0fde1db6ba53829ad0af13514c4fa3
resonar09 <darrenperez@gmail.com>
2018-03-19 10:14:21 -0400

    Change DynForm Service Mock

--- Services/DynamicFormDataMock.cs
+++ Services/DynamicFormDataMock.cs
@@ -8,36 +8,57 @@ using System.Threading;
 using Microsoft.AspNetCore.Http;
 using System.Reflection;
 using PARiConnect.MVCApp.Models.DynamicFormModels;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PARiConnect.MVCApp.Services
 {
     public class DynamicFormDataMock : IDynamicFormData
     {
-        public DynamicFormDataMock()
+        private IServiceProvider _serviceProvider;
+        public DynamicFormDataMock(IServiceProvider serviceProvider)
         {
+             _serviceProvider = serviceProvider;
         }
-        public async Task<IEnumerable<Input>> GetInputsAsync(string model)
-        {
-            Type type = Assembly.GetEntryAssembly().GetType("PARiConnect.MVCApp.Models.DynamicFormModels."+ model);
- 
-            var modelInstance = Activator.CreateInstance(type);
-            //var object = type.InvokeMember("GetModel", new IEnumerable<Input> input);
-            PropertyInfo inputsPropertyInfo = type.GetProperty("Inputs");
-            IEnumerable<Input> inputs = (IEnumerable<Input>)inputsPropertyInfo.GetValue(modelInstance, null);
 
-            return await Task.Run(() => inputs);
-        }
-        public async Task<Settings> GetSettingsAsync(string model)
+        public Task<IEnumerable<Input>> GetInputsAsync(string model)
         {
-            Type type = Assembly.GetEntryAssembly().GetType("PARiConnect.MVCApp.Models.DynamicFormModels."+ model);
- 
-            var modelInstance = Activator.CreateInstance(type);
-            //var object = type.InvokeMember("GetModel", new IEnumerable<Input> input);
-            PropertyInfo settingsPropertyInfo = type.GetProperty("Settings");
-            Settings settings = (Settings)settingsPropertyInfo.GetValue(modelInstance, null);
-
-            return await Task.Run(() => settings);
+            var modelServices = _serviceProvider.GetServices<IDynamicFormModel>();
+            var modelService = modelServices.First(o => o.GetName() == model);
+            return modelService.GetInputsAsync();
         }
+
+        public Task<Settings> GetSettingsAsync(string model)
+        {
+            var modelServices = _serviceProvider.GetServices<IDynamicFormModel>();
+            var modelService = modelServices.First(o => o.GetName() == model);
+            
+            return modelService.GetSettingsAsync();
+        }
+
+
+
+        /*         public async Task<IEnumerable<Input>> GetInputsAsync(string model)
+                {
+                    Type type = Assembly.GetEntryAssembly().GetType("PARiConnect.MVCApp.Models.DynamicFormModels."+ model);
+
+                    var modelInstance = Activator.CreateInstance(type);
+                    //var object = type.InvokeMember("GetModel", new IEnumerable<Input> input);
+                    PropertyInfo inputsPropertyInfo = type.GetProperty("Inputs");
+                    IEnumerable<Input> inputs = (IEnumerable<Input>)inputsPropertyInfo.GetValue(modelInstance, null);
+
+                    return await Task.Run(() => inputs);
+                }
+                public async Task<Settings> GetSettingsAsync(string model)
+                {
+                    Type type = Assembly.GetEntryAssembly().GetType("PARiConnect.MVCApp.Models.DynamicFormModels."+ model);
+
+                    var modelInstance = Activator.CreateInstance(type);
+                    //var object = type.InvokeMember("GetModel", new IEnumerable<Input> input);
+                    PropertyInfo settingsPropertyInfo = type.GetProperty("Settings");
+                    Settings settings = (Settings)settingsPropertyInfo.GetValue(modelInstance, null);
+
+                    return await Task.Run(() => settings);
+                } */
 
     }
 }
