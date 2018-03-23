680ca17d3438d712b795ac51aaac5d41932dd638
resonar09 <darrenperez@gmail.com>
2018-03-23 19:55:28 -0400

    Added Reports

---
+++ Services/IReportData.cs
@@ -0,0 +1,14 @@
+﻿using PARiConnect.MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARiConnect.MVCApp.Services
+{
+    public interface IReportData
+    {
+        Task<IEnumerable<Report>> GetAll();
+        Task<IEnumerable<Report>> GetListAsync();
+    }
+}
