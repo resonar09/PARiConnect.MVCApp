73598d233bb3b22bf55cff4d29aa8954d276e12f
resonar09 <darrenperez@gmail.com>
2018-04-03 18:22:26 -0400

    Consolidate Javascript

---
+++ wwwroot/js/PAR-common.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function() {
+  // Popover functions
+  $("[data-toggle=popover]").each(function(i, obj) {
+    $(this).popover({
+      html: true,
+
+      content: function() {
+        var id = $(this).attr("id");
+        return $("#popover-content-" + id).html();
+      }
+    });
+  });
+  $(".popover-dismiss").popover({
+    trigger: "focus"
+  });
+});
+function findObjectByKey(array, key, value) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i][key] === value) {
+      return array[i];
+    }
+  }
+  return null;
+}
---
+++ wwwroot/js/PAR-dynamic-form.js
@@ -0,0 +1,18 @@
+﻿function getPermissions(val,controller,action) {
+    var action = controller + "/" + action + "/" + val ;
+    var url = window.location.href + action;
+    $.ajax({
+                type: "GET",
+                url: url,
+                contentType: "application/json; charset=utf-8",
+                data: { value: val },
+                dataType: "json",
+                success: function(result) { 
+                        result.forEach((data, index) => {
+                            console.log(data.permissionID + " " + data.permissionParameterValue);
+                            $("input[name='"+data.permissionID+"'][value='"+data.permissionParameterValue+"']").prop('checked', true);
+                        }); 
+                    },
+                error: function() { alert('Error'); }
+            });
+}
